75ab927be5318fef5450d50a0bb689accd9df185
Srijak Gorai <119062466+SrijakGorai@users.noreply.github.com>
2025-09-08 17:26:30 +0530

    Update AddMaterialPage.jsx

--- stock-management/stock-management-frontend/src/pages/AddMaterialPage.jsx
+++ stock-management/stock-management-frontend/src/pages/AddMaterialPage.jsx
@@ -1,13 +1,42 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import MaterialForm from "../components/MaterialForm";
+import { Box, Paper } from "@mui/material";
 
 const AddMaterialPage = () => {
-  const dummyRefresh = () => {}; // We can ignore refresh here
+  const dummyRefresh = () => {}; // No need refresh here
+
   return (
     <>
       <Navbar />
-      <MaterialForm refresh={dummyRefresh} />
+      <Box
+        sx={{
+          minHeight: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundImage:
+            "url('https://images.unsplash.com/photo-1503387762-592deb58ef4e?auto=format&fit=crop&w=1920&q=80')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundAttachment: "fixed",
+          p: 3,
+        }}
+      >
+        <Paper
+          elevation={6}
+          sx={{
+            p: 4,
+            borderRadius: 3,
+            maxWidth: 600,
+            width: "100%",
+            backdropFilter: "blur(12px)",
+            backgroundColor: "rgba(255, 255, 255, 0.85)", // works for light/dark
+          }}
+        >
+          <MaterialForm refresh={dummyRefresh} />
+        </Paper>
+      </Box>
     </>
   );
 };
